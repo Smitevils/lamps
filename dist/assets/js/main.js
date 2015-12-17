edd36b0fd5bf2ad49ab890412b30fbf7698ed3b4
smitevils <smitevils@yandex.ru>
2015-12-17 15:15:40 +0300

    Добавил fancybox

---
+++ dist/assets/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+// Подключаем fancybox
+$(".fancybox").fancybox();
+//$.fancybox.open("http://www.vsevgallery.ru/img/bPIC/201104/201104343574.jpg")
+//$.fancybox( {href : 'http://www.bestfon.com.ua/file/16075/728x408/16:9/%D0%9A%D0%BE%D1%84%D0%B5-%D1%81-%D0%BA%D0%B5%D0%BA%D1%81%D0%BE%D0%BC_1400447656.jpg', title : 'Кофеек'} );
+//$.fancybox( ["http://widefon.com/_ld/206/85864006.jpg", 'http://www.radionetplus.ru/uploads/posts/2012-12/1355892128_vlyublennym-v-kofe-14.jpg'] );
+//$.fancybox( '<h1>Lorem lipsum</h1>' );
+$.fancybox.open('#modal-1');
+
+});
