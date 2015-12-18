333fd1ddc0406c9d6a57d7c22fa2a921b6031622
smitevils <smitevils@yandex.ru>
2015-12-18 11:10:16 +0300

    Верстаю меню

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -6,6 +6,6 @@ $(".fancybox").fancybox();
 //$.fancybox( {href : 'http://www.bestfon.com.ua/file/16075/728x408/16:9/%D0%9A%D0%BE%D1%84%D0%B5-%D1%81-%D0%BA%D0%B5%D0%BA%D1%81%D0%BE%D0%BC_1400447656.jpg', title : 'Кофеек'} );
 //$.fancybox( ["http://widefon.com/_ld/206/85864006.jpg", 'http://www.radionetplus.ru/uploads/posts/2012-12/1355892128_vlyublennym-v-kofe-14.jpg'] );
 //$.fancybox( '<h1>Lorem lipsum</h1>' );
-$.fancybox.open('#modal-1');
+//$.fancybox.open('#modal-1');
 
 });
