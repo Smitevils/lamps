d24c285b5ade13cefc6f2bbff7a0774b025a667b
smitevils <smitevils@yandex.ru>
2015-12-22 19:06:09 +0300

    Закончил главное меню

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -9,9 +9,64 @@ $(".fancybox").fancybox();
 //$.fancybox.open('#modal-1');
 
 // Кнопка каталога в меню
-$('.inner-menu').click(function(event) {
+$('.inner-menu a').click(function(event) {
 	event.preventDefault();
-	$(this).toggleClass('open');
+	$(this).parent().toggleClass('open');
 });
+//
+$(document).click(function(event) {
+	if ($(event.target).closest(".inner-menu").length) return;
+	$('.inner-menu').removeClass('open');
+	event.stopPropagation();
+});
+
+// basket popup slider
+
+$('#basket-popup-slider').slick({
+	vertical: true,
+	infinite: false,
+	arrows: true,
+	swipe: false,
+	slidesToShow: 3,
+	slidesToScroll: 3,
+	adaptiveHeight: true,
+	speed: 100,
+	prevArrow: $('.slick-prev'),
+	nextArrow: $('.slick-next')
+});
+
+// remove slide
+
+$('.basket-popup-close-btn').on('click', function(e) {
+	e.preventDefault();
+	var slideNumber = $(this).parents('.basket-main-popup__slide').index()
+	console.log("удален товар в слайдере с индексом " + slideNumber)
+	$('#basket-popup-slider').slick('slickRemove', slideNumber); //slideIndex
+		if (slideIndex !== 0){
+		slideIndex--;
+	}
+});
+
+// show basket popup
+
+var bmp = $('.basket-main-popup'); //basket_main_popup
+
+$('.go-to-cart').hover(function() {
+	//bmp.css('visibility', 'visible');
+	bmp.addClass('open');
+}, function() {
+	//bmp.css('visibility', 'hidden');
+	bmp.removeClass('open');
+});
+
+bmp.hover(function() {
+	//bmp.css('visibility', 'visible');
+	bmp.addClass('open');
+}, function() {
+	//bmp.css('opacity', '0.1').delay(3000).css('visibility', 'hidden');
+	bmp.removeClass('open');
+});
+
+
 
 });
